ec56f9be80f64c9255e6a6bfc576d9a46f478b1f
Vyom Desai <52405504+v0iid@users.noreply.github.com>
2019-11-24 16:22:43 +0530

    Selenium.py: cleaned import and add comments

--- Spammer.py
+++ Spammer.py
@@ -1,35 +1,35 @@
+# Import Selenium and webdriver
 import selenium
 from selenium import webdriver
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-# import time
 
+# Declare Bot
 botDriver = webdriver.Chrome(executable_path = 'C:\chromedriver')
 botDriver.get('https://web.whatsapp.com/')
 
-
+# Some needed variables
 continueChatBool = True
 Exitbox = 'N'
 
+# Loop for continuity of program
 while continueChatBool:
-    ChatName = input('Enter the person or group name you want to target: ')
-    ChatMsg = input('Enter Chat MEssage: ')
-    NOCount = int(input('Enter Count: '))
-    input('Enter any key to continue...')
+    ChatName = input('Enter the person or group name you want to target: ') # Chat name input
+    ChatMsg = input('Enter Chat MEssage: ') # Chat message input
+    NOCount = int(input('Enter Count: ')) # Number of times spam input
+    input('Enter any key to continue...') # Any input to continue
 
-    user = botDriver.find_element_by_xpath('//span[@title="{}"]'.format(ChatName))
+    user = botDriver.find_element_by_xpath('//span[@title="{}"]'.format(ChatName)) # Guiding bot to victim chat/group
     user.click()
     msg_box = botDriver.find_element_by_class_name('_3u328')
 
+# Spammer
     for i in range(NOCount):
         msg_box.send_keys(ChatMsg)
         button = botDriver.find_element_by_class_name('_3M-N-')
         button.click()
         print('Working...Please Wait')
     Exitbox = input('Do you want to exit?(Y or N)')
+    
+    # Simple code for a continuity option
     if Exitbox == 'Y' or Exitbox == 'y':
         continueChatBool = False
     else:
